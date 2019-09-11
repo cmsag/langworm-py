1456ceb0220dd37a7342740729ebe8dbabbe37d0
Carlos <caustin-gonzalez@britishmuseum.org>
2019-09-11 13:44:39 +0100

    Added audio test work in progress

--- src/vSynth/soundSystem.py
+++ src/vSynth/soundSystem.py
@@ -1,4 +1,4 @@
-from pyaudio import PyAudio
+from pyaudio import PyAudio, paFloat32
 import random
 import src.tools.filter as fl
 
@@ -19,9 +19,13 @@ class SoundSystem:
 
         self.block_time = block_length / self.sample_rate
 
-    def start_sound(self, duration):
+        self.create_white_noise_node(2)
+
+    def start_sound(self, *duration):
         # default = create 2 seconds of noise
-        duration = 2
+        if duration is None:
+            duration = 2
+
         white_noise = self.create_white_noise_node(duration * self.sample_rate)
 
         aspirate_filter = fl.BPBiquadFilter(centre_frequency=500,
@@ -34,28 +38,35 @@ class SoundSystem:
         # Function WiP
         # ...
 
-
     def create_white_noise_node(self, frame_count):
         # Using a PortAudio stream via pyaudio to replace the web audiocontext createBuffer function.
         # https://people.csail.mit.edu/hubert/pyaudio/docs/#class-stream
         # Not sure if it will work...
         # May be input or output. Putting output for now.
         input_node = PyAudio()
-        my_stream = input_node.open(input=True,
-                           channels=1,
-                           frames_per_buffer=frame_count,
-                           rate=self.sample_rate)
+        my_stream = input_node.open(format=paFloat32,
+                                    input=True,
+                                    channels=1,
+                                    frames_per_buffer=frame_count,
+                                    rate=self.sample_rate)
 
         now_buffering = my_stream.read(1)
         i = 0
         while i < frame_count:
             i += 1
-            now_buffering[i] = random.randint()
+            now_buffering[i] = random.randint(0,44100)
+
+        # AudioBufferSourceNode.buffer
+        # An AudioBuffer that defines the audio asset to be played, or when set to the value null,
+        # defines a single channel of silence (in which every sample is 0.0).
+
+        # var source = this.audioContext.createBufferSource();
+        # source.buffer = myArrayBuffer;
+        # source.loop = true;
 
         source = PyAudio().open(output=True,
                                 )
-        source.buffer = my_stream
-        source.loop = True
+        source.input_host_api_specific_stream_info = my_stream
 
         return source
         # For this we need to get the equivalent of an audio buffer source node
@@ -64,3 +75,5 @@ class SoundSystem:
         # ...
         # Function WiP
         # ...
+
+test = SoundSystem(default_sr)
---
+++ test/audiotest.py
@@ -0,0 +1,60 @@
+# https://people.csail.mit.edu/hubert/pyaudio/docs/#pyaudio.Stream.__init__
+
+# https://stackoverflow.com/questions/24920346/filtering-a-wav-file-using-python
+
+from pyaudio import PyAudio, paFloat32
+import numpy as np
+import random
+import math
+
+class BPBiquadFilter:
+
+    def __init__(self, centre_frequency, sample_rate):
+
+        dBgain = 20
+        Q = 0.5
+        self.sample_rate = sample_rate
+        self.centre_frequency = centre_frequency
+
+        absolute_gain = 10**(dBgain / 40)
+
+        omega = 2 * math.pi * self.centre_frequency/sample_rate
+
+        omega_sn = math.sin(omega)
+        omega_cs = math.cos(omega)
+
+        alpha = omega_sn / (2*Q)
+
+        beta = math.sqrt(2*absolute_gain)
+
+        self.b0 = alpha
+        self.b1 = 0
+        self.b2 = -alpha
+        self.a0 = 1 + alpha
+        self.a1 = -2 * omega_cs
+        self.a2 = 1 - alpha
+
+bp = BPBiquadFilter(centre_frequency=300,sample_rate=44100)
+
+samples = np.array([bp.b0,bp.b1,bp.b2,bp.a0,bp.a1,bp.a2])
+
+volume = 0.5
+#samples = (np.sin(2*np.pi*np.arange(44100*1)*440/44100)).astype(np.float32)
+print(type(samples))
+
+pa = PyAudio()
+my_stream = pa.open(format=paFloat32,
+                    # the 3 arguments from createWhiteNoiseNode in pinktrombone
+                    channels=1,
+                    frames_per_buffer=8,
+                    rate=44100,
+                    # End of pinktrombone args
+                    output=True)
+
+while True:
+    play(volume*samples)
+
+my_stream.stop_stream()
+my_stream.close()
+
+pa.terminate()
